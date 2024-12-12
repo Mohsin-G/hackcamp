c8591c519cb512a626d302c4b9e00bce090fba75
Mahfuz <m.ahmed79@edu.salford.ac.uk>
2024-12-12 22:13:55 +0000

    Added extra details to the cards and added new CSS to the upload button

--- Models/imageModel.php
+++ Models/imageModel.php
@@ -4,20 +4,32 @@ class ImageModel {
         return [
             [
                 'src' => '/images/bottle.jpg',
-                'title' => 'Bottle',
-                'description' => 'In this project, we have used <strong>50</strong>  pictures.',
+                'title' => 'Model 1: Bottle',
+                'description' => '<ul>
+                 <li>Dataset of 50 pictures</li>
+                 <li>File Size: 1.48MB</li>
+                 <li>Maximum distance away from the object: 50cm</li>
+                 </ul>',
                 'url' => 'Display.php?model=bottle50pic.splat'
             ],
             [
                 'src' => '/images/chair.jpg',
-                'title' => 'Chair',
-                'description' => 'In this project, we have used <strong>100</strong> pictures.',
+                'title' => 'Model 2: Chair',
+                'description' =>'<ul>
+                 <li>Dataset of 100 pictures</li>
+                 <li>File Size: 16.5MB</li>
+                 <li>Maximum distance away from the object: 80cm</li>
+                 </ul>',
                 'url' => 'Display.php?model=chair100pic.splat'
             ],
             [
                 'src' => '/images/bench.jpg',
-                'title' => 'Bench',
-                'description' => 'In this project, we have used <strong>500</strong> pictures.',
+                'title' => 'Model 3: Bench',
+                'description' => '<ul>
+                 <li>Dataset of 500 pictures</li>
+                 <li>File Size: 93.1MB</li>
+                 <li>Maximum distance away from the object: 2m</li>
+                 </ul>',
                 'url' => 'Display.php?model=bench500pic.splat'
             ],
         ];
